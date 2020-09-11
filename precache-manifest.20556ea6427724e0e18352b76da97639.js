2854fbc6b3bb0e1f4da5fa587d47d455ac361ef2
pritamInnoraft <pritam.sadhu@innoraft.com>
2020-09-11 12:22:29 +0530

    Updates

---
+++ precache-manifest.20556ea6427724e0e18352b76da97639.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d2cfdc7f79fe6c9543a354d092f6be",
+    "url": "/flashLight/index.html"
+  },
+  {
+    "revision": "8c40b1b58e99c318d4d8",
+    "url": "/flashLight/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "650c765feff85fcc8955",
+    "url": "/flashLight/static/js/2.e4ebc84d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flashLight/static/js/2.e4ebc84d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c40b1b58e99c318d4d8",
+    "url": "/flashLight/static/js/main.d049f15f.chunk.js"
+  },
+  {
+    "revision": "4ea7966e6579c1a6b843",
+    "url": "/flashLight/static/js/runtime-main.23ee17bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/flashLight/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/flashlight/precache-manifest.e37ab31eb1c1bffd43dcb83849dd7147.js"
+  "/flashLight/precache-manifest.20556ea6427724e0e18352b76da97639.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flashlight/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flashLight/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
