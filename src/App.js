223d39137b69ccc0e4ec2762ef1614f294ee4ec0
pritamInnoraft <pritam.sadhu@innoraft.com>
2020-09-11 12:12:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const cameraInitialization = () => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: {
+          facingMode: "environment",
+        },
+      })
+      .then((stream) => {
+        const video = document.querySelector("video");
+        video.srcObject = stream;
+
+        // get the active track of the stream
+        const track = stream.getVideoTracks()[0];
+
+        video.addEventListener("loadedmetadata", (e) => {
+          window.setTimeout(
+            () => onCapabilitiesReady(track.getCapabilities()),
+            500
+          );
+        });
+
+        function onCapabilitiesReady(capabilities) {
+          if (capabilities.torch) {
+            track
+              .applyConstraints({
+                advanced: [{ torch: true }],
+              })
+              .catch((e) => console.log(e));
+          }
+        }
+      })
+      .catch((err) => console.error("getUserMedia() failed: ", err));
+  };
+
+  useEffect(() => {
+    cameraInitialization();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <video autoPlay id="video"></video>
+      </div>
     </div>
   );
 }
