b7a70c8f924c1030bb2a6324a5befbeac7ade33d
pritamInnoraft <pritam.sadhu@innoraft.com>
2020-09-11 16:42:18 +0530

    image with flash integrated.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import CameraPhoto, { FACING_MODES } from "jslib-html5-camera-photo";
 
 function App() {
+  const [img, setImg] = useState(null);
   const cameraInitialization = () => {
     navigator.mediaDevices
       .getUserMedia({
@@ -36,15 +38,77 @@ function App() {
       })
       .catch((err) => console.error("getUserMedia() failed: ", err));
   };
+  let cameraPhoto;
+
+  const ccc = () => {
+    let videoElement = document.getElementById("video");
+    cameraPhoto = new CameraPhoto(videoElement);
+
+    cameraPhoto
+      ._getStreamDevice(FACING_MODES.ENVIRONMENT, {})
+      .then((stream) => {
+        const video = document.querySelector("video");
+        video.srcObject = stream;
+
+        // get the active track of the stream
+        const track = stream.getVideoTracks()[0];
+
+        video.addEventListener("loadedmetadata", (e) => {
+          window.setTimeout(
+            () => onCapabilitiesReady(track.getCapabilities()),
+            500
+          );
+        });
+
+        function onCapabilitiesReady(capabilities) {
+          console.log("heyyyy", capabilities);
+          if (capabilities.torch) {
+            track
+              .applyConstraints({
+                advanced: [{ torch: true }],
+              })
+              .catch((e) => console.log(e));
+          }
+        }
+      })
+      .catch((err) => console.error("getUserMedia() failed: ", err));
+
+    cameraPhoto
+      .startCamera(FACING_MODES.ENVIRONMENT)
+      .then(() => {
+        console.log("Camera started !");
+      })
+      .catch((error) => {
+        console.error("Camera not started!", error);
+      });
+  };
 
   useEffect(() => {
-    cameraInitialization();
+    // cameraInitialization();
+    ccc();
   }, []);
+
+  const takePic = () => {
+    const config = {
+      sizeFactor: 1,
+    };
+    let dataUri = cameraPhoto.getDataUri(config);
+    console.log(dataUri);
+    setImg(dataUri);
+    cameraPhoto.stopCamera();
+  };
 
   return (
     <div className="App">
       <div>
-        <video autoPlay id="video"></video>
+        {img !== null ? (
+          <img src={img} alt="hey" />
+        ) : (
+          <div>
+            <video autoPlay id="video"></video>
+            <button onClick={takePic}>Take Pic</button>
+          </div>
+        )}
       </div>
     </div>
   );
